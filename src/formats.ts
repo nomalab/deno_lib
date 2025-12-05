2cc6eb712bd833d683725edfdc2a728b4ebe7b1f
Maxime Dantec <hello@warry.fr>
2025-12-05 15:29:01 +0100

    json -> ts because vite

--- src/formats.ts
+++ src/formats.ts
@@ -1,5 +1,5 @@
-import versionsJson from "./versions.json" with { type: "json" };
-import layoutsJson from "./layouts.json" with { type: "json" };
+import versionsJson from "./versions.ts"
+import layoutsJson from "./layouts.ts"
 
 export const versions = versionsJson;
 export type Version = keyof (typeof versions);
